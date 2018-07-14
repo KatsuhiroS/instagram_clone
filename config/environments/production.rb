cc3ca6badc001d9628dbee060c8e31536e12bd4a
Katsuhiro <katsuhiro@MacBookPro.local>
2018-07-14 17:17:58 +0900

    Action_Mailerのhost変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'enigmatic-falls-57466.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'piscine-bastille-67854.herokuapp.com' }
   config.action_mailer.delivery_method = :letter_opener_web
 end
